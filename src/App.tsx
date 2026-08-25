358e2f348f2ef94ccc9ad9f7d6444be5f15caea9
Samir <samirstown@hotmail.com>
2026-08-25 05:17:48 -0700

    Added AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,31 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import { AuthProvider } from './context/AuthContext';
+import Cart from './pages/Cart';
+import Register from './pages/Cart';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+import Profile from './pages/Profile';
+
 
 function App() {
   return (
     <>
       <Navbar />
       <div className="container mt-4">
+        <AuthProvider>
+        <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
+        </BrowserRouter>
+        </AuthProvider>
       </div>
     </>
   );
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,37 @@
+import { createContext,useState, useEffect, useContext, type ReactNode } from "react";
+import { onAuthStateChanged, type User } from "firebase/auth";
+import {auth} from "../lib/firebase/firebase"
+
+interface AuthContextType {
+    user: null | User,
+    setUser: (user:User) => void
+}
+const AuthContext = createContext<AuthcontextType> ({ 
+    user: null,
+    setUser: (user:User) => {}
+})
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, setUser }}>
+            {children}
+        </AuthContext.Provider>    
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
+export default AuthContext;
